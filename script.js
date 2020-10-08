f5ae19f0e47c80af83bcea46509a41bebf063886
Darin Wallace <dwalleye66@gmail.com>
2020-10-08 18:22:14 -0500

    OnTime

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+// Quiz questions
+var quizQuestions = [{
+    question: "Inside which HTML element do we put the JavaScript?",
+    choiceA: "<script>",
+    choiceB: "<scripting>",
+    choiceC: "<js>",
+    choiceD: "<javascript>",
+    correctAnswer: "a"},
+   {
+    question: "Where is the correct place to insert a JavaScript?",
+    choiceA: "The <body> section",
+    choiceB: "The <head> section",
+    choiceC: "Anywhere you please",
+    choiceD: "Both the <head> section and the <body> section are correct",
+    correctAnswer: "d"},
+   {
+    question: "How to write an IF statement in JavaScript?",
+    choiceA: "if i = 5 then",
+    choiceB: "if (i == 5)",
+    choiceC: "if i = 5",
+    choiceD: "if i == 5 then",
+    correctAnswer: "b"},
+   {
+    question: "How can you add a comment in a JavaScript?",
+    choiceA: "<--This is a comment--",
+    choiceB: "'This is a comment",
+    choiceC: "//This is a comment",
+    choiceD: "<!--This is a comment-->",
+    correctAnswer: "c"},
+   {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choiceA: "onchange",
+    choiceB: "onclick",
+    choiceC: "onmouseover",
+    choiceD: "onmouseclick",
+    correctAnswer: "a"}]
