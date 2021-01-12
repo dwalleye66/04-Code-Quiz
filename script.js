12de5dc16a4d26fc08a146c846110bf81ef4574f
Darin Wallace <dwalleye66@gmail.com>
2021-01-11 21:36:17 -0600

    completing imcomplete

--- script.js
+++ script.js
@@ -1,39 +1,206 @@
-
-
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var highscoreDiv = document.getElementById("high-scorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highscore-initials");
+var endGameBtns = document.getElementById("endGameBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("highscore-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
 
 // Quiz questions
-var quizQuestions = [{
-    question: "Inside which HTML element do we put the JavaScript?",
-    choiceA: "<script>",
-    choiceB: "<scripting>",
-    choiceC: "<js>",
-    choiceD: "<javascript>",
-    correctAnswer: "a"},
-   {
-    question: "Where is the correct place to insert a JavaScript?",
-    choiceA: "The <body> section",
-    choiceB: "The <head> section",
-    choiceC: "Anywhere you please",
-    choiceD: "Both the <head> section and the <body> section are correct",
-    correctAnswer: "d"},
-   {
-    question: "How to write an IF statement in JavaScript?",
-    choiceA: "if i = 5 then",
-    choiceB: "if (i == 5)",
-    choiceC: "if i = 5",
-    choiceD: "if i == 5 then",
-    correctAnswer: "b"},
-   {
-    question: "How can you add a comment in a JavaScript?",
-    choiceA: "<--This is a comment--",
-    choiceB: "'This is a comment",
-    choiceC: "//This is a comment",
-    choiceD: "<!--This is a comment-->",
-    correctAnswer: "c"},
-   {
-    question: "Which event occurs when the user clicks on an HTML element?",
-    choiceA: "onchange",
-    choiceB: "onclick",
-    choiceC: "onmouseover",
-    choiceD: "onmouseclick",
-    correctAnswer: "a"}]
+var quizQuestions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choiceA: "script",
+        choiceB: "scripting",
+        choiceC: "js",
+        choiceD: "javascript",
+        correctAnswer: "a"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        choiceA: "The body section",
+        choiceB: "The head section",
+        choiceC: "Anywhere you please",
+        choiceD: "Both the head section and the body section are correct",
+        correctAnswer: "d"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choiceA: "if i = 5 then",
+        choiceB: "if (i == 5)",
+        choiceC: "if i = 5",
+        choiceD: "if i == 5 then",
+        correctAnswer: "b"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choiceA: "<--This is a comment--",
+        choiceB: "'This is a comment",
+        choiceC: "//This is a comment",
+        choiceD: "This is a comment--",
+        correctAnswer: "c"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choiceA: "onchange",
+        choiceB: "onclick",
+        choiceC: "onmouseover",
+        choiceD: "onmouseclick",
+        correctAnswer: "a"
+    }
+
+];
+
+var finalQuestionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 76;
+var timerInterval;
+var score = 0;
+var correct;
+
+// This function cycles through the object array containing the quiz questions to generate the questions and answers.
+function generateQuizQuestion() {
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex) {
+        return showScore();
+    }
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    buttonA.innerHTML = currentQuestion.choiceA;
+    buttonB.innerHTML = currentQuestion.choiceB;
+    buttonC.innerHTML = currentQuestion.choiceC;
+    buttonD.innerHTML = currentQuestion.choiceD;
+};
+
+// Start Quiz function starts the TimeRanges, hides the start button, and displays the first quiz question.
+function startQuiz() {
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuizQuestion();
+
+    //Timer
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        quizTimer.textContent = "Time left: " + timeLeft;
+
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            showScore();
+        }
+    }, 1000);
+    quizBody.style.display = "block";
+}
+
+function showScore() {
+    quizBody.style.display = "none"
+    gameoverDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    highscoreInputName.value = "";
+    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+
+submitScoreBtn.addEventListener("click", function highscore() {
+
+
+    if (highscoreInputName.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    } else {
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = highscoreInputName.value.trim();
+        var currentHighscore = {
+            name: currentUser,
+            score: score
+        };
+
+        gameoverDiv.style.display = "none";
+        highscoreContainer.style.display = "flex";
+        highscoreDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        generateHighscores();
+
+    }
+
+});
+
+// This function clears the list for the high scores and generates a new high score list from local storage
+function generateHighscores() {
+    highscoreDisplayName.innerHTML = "";
+    highscoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i = 0; i < highscores.length; i++) {
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        highscoreDisplayName.appendChild(newNameSpan);
+        highscoreDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+// This function displays the high scores page 
+function showHighscore() {
+    startQuizDiv.style.display = "none"
+    gameoverDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    highscoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+// This function clears the local storage of the high scores 
+function clearScore() {
+    window.localStorage.clear();
+    highscoreDisplayName.textContent = "";
+    highscoreDisplayScore.textContent = "";
+}
+
+// Enable replay
+function replayQuiz() {
+    highscoreContainer.style.display = "none";
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "flex";
+    timeLeft = 76;
+    score = 0;
+    currentQuestionIndex = 0;
+}
+
+// Validate response to each answer 
+function checkAnswer(answer) {
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex) {
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //If correct.
+    } else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex) {
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //If wrong.
+    } else {
+        showScore();
+    }
+}
+
+// Start the quiz!
+startQuizButton.addEventListener("click", startQuiz);
